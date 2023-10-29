1add39ce67894b6a86fbd7616c19e71249f4cd06
danwhitlock <110103983+danwhitlock@users.noreply.github.com>
2023-10-29 12:54:49 +0000

    create generate password function, update comments

--- script.js
+++ script.js
@@ -1,32 +1,3 @@
-// Array of special characters to be included in password
-var specialCharacters = [
-  '@',
-  '%',
-  '+',
-  '\\',
-  '/',
-  "'",
-  '!',
-  '#',
-  '$',
-  '^',
-  '?',
-  ':',
-  ',',
-  ')',
-  '(',
-  '}',
-  '{',
-  ']',
-  '[',
-  '~',
-  '-',
-  '_',
-  '.'
-];
-
-// Array of numeric characters to be included in password
-var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
 // Array of lowercase characters to be included in password
 var lowerCasedCharacters = [
@@ -88,25 +59,69 @@ var upperCasedCharacters = [
   'Z'
 ];
 
-var passwordLength;       // variable to store the chosen password length
-var lowerChars;           // variable to store whether lower case characters have been requested
-var upperChars;           // variable to store whether upper case characters have been requested
-var numericChars;         // variable to store whether numeric characters have been requested
-var specialChars;         // variable to store whether special characters have been requested
-var chosenChars = [];     // array to store every possible character the password can contain, based upon the users choices
+// Array of numeric characters to be included in password
+var numericCharacters = [
+  '0',
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9'];
+
+// Array of special characters to be included in password
+var specialCharacters = [
+  '@',
+  '%',
+  '+',
+  '\\',
+  '/',
+  "'",
+  '!',
+  '#',
+  '$',
+  '^',
+  '?',
+  ':',
+  ',',
+  ')',
+  '(',
+  '}',
+  '{',
+  ']',
+  '[',
+  '~',
+  '-',
+  '_',
+  '.'
+];
+
+// variable to store the chosen password length
+var passwordLength;       
+
+// variable to store whether each of the four character types have been requested
+var lowerChars;            
+var upperChars;           
+var numericChars;         
+var specialChars;         
+
+// An array to hold a list of all characters from the options the user chose
+var chosenChars = [];     
 
 
 // Function to prompt user for password options and store chosen options in a new variable
 function getPasswordOptions() {
   passwordLength = prompt("Enter a password length between 8 and 128 characters");  // ask for desired length
 
-  // if input value is too low, too high or not a number, flag error message
+  // If input value is too low, too high or not a number, flag error message
   if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {        
     alert("You must enter a number between 8 and 128.  Please try again"); 
   }
 
-  // ask user which of the four character types they wish to include, and add the chosen characters to chosenChars
-  // using the ... (spread operator) makes sure that when added, they are all merged into a single array, rather than being added to chosenChars as separate arrays e.g. [a, B, 1] rather than [a],[B],[1]
+  // Ask user which of the four character types they wish to include, and add those characters to the chosenChars array.  Using the ... (spread operator) makes sure that when added, they are all stored as single array rather than as separate arrays e.g. [a, B, 1, @] rather than [ [a], [B], [1], [@] ]
 
   lowerChars = confirm("Include lower case characters?"); 
   console.log ("Lower Case = " + lowerChars);
@@ -132,7 +147,7 @@ function getPasswordOptions() {
     chosenChars.push(...specialCharacters);
   }
  
-   // if no option was chosen, flag error message
+   // If no option was chosen, flag error message
   if (!lowerChars && !upperChars && !numericChars && !specialChars) {
     alert("You must choose at least one password option.  Please try again");
   }
@@ -148,6 +163,17 @@ function getRandom(array) {
 // Function to generate password with user input
 function generatePassword() {
   getPasswordOptions();
+
+  // make password empty to begin with
+  password = "";
+  
+  // iterate through the password length, get a random character from the array of all chosenChars and add that to the password
+  for (var i = 0; i < passwordLength; i++) {
+    randomCharFromAllChosen = getRandom(chosenChars);
+    password += randomCharFromAllChosen;
+  }
+  // return the password outside of the loop, otherwise it'll stop after the first character
+  return password;
 };
 
 
